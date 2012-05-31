d558e70e460d7dabf759b866f9f37a12ad78dd54
loye <loye.qiu@gmail.com>
2012-05-31 17:57:26 +0800

    first time check in

---
+++ CNChar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CNChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tran = new Translator();
+            Console.WriteLine("请输入汉字:\n");
+            while (true)
+            {
+                Console.WriteLine(tran.GetChineseSpell(Console.ReadLine()));
+            }
+        }
+    }
+}
+
+
